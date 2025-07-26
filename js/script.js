4bb6557e5852efe14ecfcd13ce3e97b4b4abe48a
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-07-26 14:58:25 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ function myFunction() {
   if (x.style.display === "block") {
     x.style.display = "none";
     y.style.display="block";
-    z.style="color: black;outline:0vmax black;"
+    z.style="color: black;outline:0vmax black"
   } else {
     y.style.display="none";
-    z.style="color: transparent;outline:calc(3.5vmax/50) black;";
+    z.style="color: transparent;outline:calc(3.5vmax/50) black";
     x.style="width:20vw;padding-top:5vw;display:block; float:left; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
   }
 }
