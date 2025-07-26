ef2a60890cef47249cfdf0b1fa716d93a0390093
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-07-26 14:46:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function myFunction() {
   var x = document.getElementById("myLinks");
   var y = document.getElementById("yes");
-  var z = document.getElementById("dot");
+  var z = document.getElementById("logo");
   if (x.style.display === "block") {
     x.style.display = "none";
-    y.style.display="inline-block";
-    z.style="font-size:5vw; color: black;-webkit-text-stroke-color: black;-webkit-text-stroke-width: 0;";
+    y.style.display="block";
+    z.style="color: black;outline:0 black;
   } else {
     y.style.display="none";
-    z.style="font-size:5vw; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
-    x.style="width:20vw;padding-top:5vw;display:inline-block; float:left; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
+    z.style="color: transparent;outline:calc(3.5vmax/50) black;";
+    x.style="width:20vw;padding-top:5vw;display:block; float:left; color: transparent;-webkit-text-stroke-color: black;-webkit-text-stroke-width: calc(3.5vmax/50);";
   }
 }
