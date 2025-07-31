ec93b39f8d295345d9b68a1fff2eab7ec73614fb
Schluecu <136748981+Schluecu@users.noreply.github.com>
2025-07-31 16:35:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,14 +5,10 @@ function myFunction() {
   if (x.style.display === "grid") {
     x.style.display="none";
     y.style.display="block";
-    z.style="width:5.5vw;height:5.5vw;min-width:50px;min-height:50px;
-    @media (prefers-color-scheme: dark) {color: gainsboro;}
-    @media (prefers-color-scheme: light) {color: black;};"
+    z.style="width:5.5vw;height:5.5vw;min-width:50px;min-height:50px;@media (prefers-color-scheme: dark) {color: gainsboro;}@media (prefers-color-scheme: light) {color: black;};"
   } else {
     y.style.display="none";
     z.style="width:5.5vw;height:5.5vw;min-width:50px;min-height:50px;color:gray;"
-    x.style="display:grid;width:40vw;padding-left:5vw;padding-top:5vmax;
-    @media (prefers-color-scheme: dark) {color: gainsboro;}
-    @media (prefers-color-scheme: light) {color: black;};
+    x.style="display:grid;width:40vw;padding-left:5vw;padding-top:5vmax;@media (prefers-color-scheme: dark) {color: gainsboro;}@media (prefers-color-scheme: light) {color: black;};
   }
 }
